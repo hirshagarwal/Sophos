09e78b244e026dc435de49435da6bb69833fb269
Hirsh Agarwal <hagarwal@andplus.com>
2017-06-28 08:39:06 -0400

    Changed Test Names
    The tests should actually run with the updated names.

--- Sophos/SophosMain.py
+++ Sophos/SophosMain.py
@@ -151,21 +151,3 @@ class Optimizer():
         gradient = X.T.dot(error) / X.shape[0]
         
         return gradient
-
-
-# In[530]:
-
-
-i = np.matrix(('1, 3'))
-model = Model()
-
-# Define Model Layers
-L1 = Layer(3, 2)
-model.add(L1)
-activation1 = Activation('sigmoid')
-model.add(activation1)
-model.add(Layer(1, 3))
-model.add(Activation('sigmoid'))
-model.train(i, 1)
-model.show()
-
--- Sophos/SophosTest.py
+++ Sophos/SophosTest.py
@@ -1,6 +1,23 @@
 import unittest
 import SophosMain
 
-class FirstTest(unittest.TestCase):
-	def test(self):
+class MainTests(unittest.TestCase):
+
+	def setUp(self):
+		# i = np.matrix(('1, 3'))
+		# model = Model()
+
+		# # Define Model Layers
+		# L1 = Layer(3, 2)
+		# model.add(L1)
+		# activation1 = Activation('sigmoid')
+		# model.add(activation1)
+		# model.add(Layer(1, 3))
+		# model.add(Activation('sigmoid'))
+		# model.train(i, 1)
+		# model.show()
+		pass
+
+
+	def test_mainTest(self):
 		self.assertEqual(1, 1)

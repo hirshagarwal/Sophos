Fixed the test for backpropagation
Backpropagation works and has been tested. Need to consider adding support for batch training.

Sophos/SophosNet.py
@@ -9,7 +9,9 @@ class Model():
     
     # Model will contain sequence of layers to feed data 
     def __init__(self):
+        # Made a list to hold all of the components of the model
         self.components = list()
+        # Set default learning rate
         self.lr = 0.5
     
     # Add a layer to the model
@@ -37,14 +39,14 @@ class Model():
         if type(self.components[len(self.components)-1]) != Activation:
             raise ValueError("Output layer must have activation function")
         
-        # Optimize
+        # Store the components as a local variable for easier access
         components = self.components
         # Store the total error
         error_total = np.sum(0.5 * np.square(Y-pred))
         self.error_total = error_total
         # Get error for output layer
         
-
+        # Begin backpropagation
         last_dE = 0
         last_delta = 0
         error = list()
@@ -106,6 +108,7 @@ class Model():
         
         return lastOut
     
+    # Display the structure of the model
     def show(self):
         model_display = ""
         for component in self.components:
@@ -114,15 +117,18 @@ class Model():
         model_display += "---------------"
         return model_display
 
+    # Feed data through all of the layers in the model
     def feed(self, X):
         lastOut = X
         for layer in self.components:
             lastOut = layer.feed(lastOut)
         return lastOut
 
+    # Get the total error of the last training point run through the model
     def getTotalError(self):
         return self.error_total
 
+    # Gets a prediction and runs the output through a step function with a threshold at 0.5
     def predictStep(self, X):
         # Step Function for prediction
         lastOut = X
@@ -132,13 +138,14 @@ class Model():
             return 1
         return 0
 
-    # Normal 
+    # Gets a raw prediction from the model
     def predict(self, X):
         lastOut = X
         for layer in self.components:
             lastOut = layer.feed(lastOut)
         return lastOut
 
+    # Set the learning rate
     def setLearningRate(self, lr):
         self.lr = lr
 

Sophos/SophosTest.py
@@ -13,28 +13,45 @@ class MainTests(unittest.TestCase):
         # Build Model
         model = sn.Model()
         l1 = sn.Layer(2, 1)
-        print(l1.getWeights())
         activation1 = sn.Activation('sigmoid')
         model.add(l1)
+        l1.setWeights(np.matrix('.5; .5; .5'))
         model.add(activation1)
         model.setLearningRate(0.1)
-        # print("Random: ", np.random.rand(2))
-        # Train Model
-        for i in range(10000):
-        # x = np.random.rand(2)
 
-            x_input = np.matrix('0 0')
-            y_input = np.matrix('0')
-            model.train(x_input, y_input)
+        # Setup data and train
+        x_input = np.matrix('0 0')
+        y_input = np.matrix('0')
+        model.train(x_input, y_input)
 
-            x_input = np.matrix('1 1')
-            y_input = np.matrix('1')
-            model.train(x_input, y_input)
-        
-            # print("Total Error: ", model.getTotalError())
-        # print("End Weights: ", l1.getWeights())
+        # First Weights
+        weights_first = l1.getWeights()
 
-    def test_TotalError(self):
+        x_input = np.matrix('1 1')
+        y_input = np.matrix('1')
+        model.train(x_input, y_input)
+
+        # Set ending weights
+        weights_end = l1.getWeights()
+
+
+        # Set expected weights
+        weights_first_expected = np.matrix('4853; 5000; 5000')
+        weights_end_expected = np.matrix('4881; 5027; 5027')
+        weights_first = (weights_first * 10000).astype(int)
+        weights_end = (weights_end * 10000).astype(int)
+        t1 = False
+        t2 = False
+        if (weights_first == weights_first_expected).all():
+            t1 = True
+        if (weights_end == weights_end_expected).all():
+            t2 = True
+
+        # Equality Tests
+        self.assertTrue(t1)
+        self.assertTrue(t2)
+
+    def test_BackpropagationMultiLayer(self):
         model = sn.Model()
         x_input = np.matrix('.05 .1')
         y = np.matrix('.01 .99')
@@ -46,20 +63,24 @@ class MainTests(unittest.TestCase):
         l2.setWeights(w2)
         activation1 = sn.Activation('sigmoid')
         activation2 = sn.Activation('sigmoid')
-        # print("Total Error Test -")
         model.add(l1)
         model.add(activation1)
         model.add(l2)
         model.add(activation2)
         original_output = model.feed(x_input)
-        for i in range(100):
-            model.train(x_input, y)
-            # print("Total Error:", model.getTotalError())
-        # print("Original Output: ", original_output)
-        # print("Trained Output: ", model.feed(x_input))
+        model.train(x_input, y)
+        updated_weights = l2.getWeights()
+        updated_weights_l1 = l1.getWeights()
+        expected_weights = np.matrix('.53075072 0.61904912; 0.35891648 0.51130127; 0.40866619 0.56137012')
+        expected_weights_l1 = np.matrix('0.34561432 0.34502287; 0.14978072 0.24975114; 0.19956143 0.29950229')
+        self.assertEqual(updated_weights_l1.all(), expected_weights_l1.all())
+        self.assertEqual(updated_weights.all(), expected_weights.all())
 
+    def test_backpropagationSingleLayer(self):
 
-    def test_Activation(self):
+        pass
+
+    def test_TotalError(self):
         model = sn.Model()
         x_input = np.matrix('.05 .1')
         y = np.matrix('.01 .99')
@@ -77,10 +98,6 @@ class MainTests(unittest.TestCase):
         model.train(x_input, y)
         # Value confirmed from online source and by hand
         self.assertEqual(model.getTotalError(), 0.29837110876000272)
-
-    def test_Training(self):
-        # TODO - Add test for training model
-        pass
 
     def test_ForwardProp(self):
         self.model = sn.Model()